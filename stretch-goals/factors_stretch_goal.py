2d5110b5526ef6608639065c13489e773d380cac
htr-volker <htr.volker@gmail.com>
2020-06-23 14:58:47 +0100

    init commit

---
+++ stretch-goals/factors_stretch_goal.py
@@ -0,0 +1,9 @@
+def factors(number):
+    # ==============
+    # Your code here
+    
+    # ==============
+
+print(factors(15)) # Should print [3, 5] to the console
+print(factors(12)) # Should print [2, 3, 4, 6] to the console
+print(factors(13)) # Should print “13 is a prime number”
---
+++ tasks/vowel_swapper.py
@@ -0,0 +1,9 @@
+def vowel_swapper(string):
+    # ==============
+    # Your code here
+
+    # ==============
+
+print(vowel_swapper("aA eE iI oO uU")) # Should print "44 33 !! ooo000 |_||_|" to the console
+print(vowel_swapper("Hello World")) # Should print "H3llooo Wooorld" to the console 
+print(vowel_swapper("Everything's Available")) # Should print "3v3ryth!ng's 4v4!l4bl3" to the console
