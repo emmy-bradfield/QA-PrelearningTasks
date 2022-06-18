d678eb084b04fac4eaf9e6b04705424c02e59527
emmy-bradfield <107776677+emmy-bradfield@users.noreply.github.com>
2022-06-19 00:18:46 +0100

    push to validate

--- stretch-goals/vowel_swapper_stretch_goal.py
+++ stretch-goals/vowel_swapper_stretch_goal.py
@@ -1,7 +1,17 @@
 def vowel_swapper(string):
     # ==============
     # Your code here
-
+    new_string=string.replace('A', '1')
+    string=new_string
+    new_string=string.replace('E', '3')
+    string=new_string
+    new_string=string.replace('I', '!')
+    string=new_string
+    new_string=string.replace('O', '000')
+    string=new_string
+    new_string=string.replace('U','|_|')
+    string=new_string
+    return string
     # ==============
 
 print(vowel_swapper("aAa eEe iIi oOo uUu")) # Should print "a/\a e3e i!i o000o u\/u" to the console
