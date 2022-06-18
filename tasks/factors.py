306c340352f62dba25d03d68d229c2f883e7a9fc
emmy-bradfield <107776677+emmy-bradfield@users.noreply.github.com>
2022-06-18 23:36:42 +0100

    push to validate

--- tasks/factors.py
+++ tasks/factors.py
@@ -1,7 +1,10 @@
 def factors(number):
     # ==============
     # Your code here
-    
+   print("The factors of",number,"are:")
+   for i in range(1, number + 1):
+       if number % i == 0:
+           print(i)
     # ==============
 
 print(factors(15)) # Should print [3, 5] to the console
--- tasks/vowel_swapper.py
+++ tasks/vowel_swapper.py
@@ -1,12 +1,8 @@
 def vowel_swapper(string):
     # ==============
     # Your code here
-    string.replace('a', '4')
-    string.replace('e', '3')
-    string.replace('i', '!')
-    string.replace('o', 'ooo')
-    string.replace('O', '000')
-    string.replace('u', '|_|')
+    string.replace('a', '4').replace('e', '3').replace('i', '!').replace('o', 'ooo').replace('O', '000').replace('u', '|_|')
+    return vowel_swapper
     # ==============
 
 print(vowel_swapper("aA eE iI oO uU")) # Should print "44 33 !! ooo000 |_||_|" to the console
